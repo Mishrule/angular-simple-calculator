8035de2088f8b81727d2baec6886ad8a5aa43bd5
Mishrule <mishrule1@gmail.com>
2019-06-23 19:02:45 +0000

    Complete Calcuation with Angular

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  public number1: number;
+  public number2: number;
+  public result: number;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addition() {
+    this.result = this.number1 + this.number2;
+  }
+
+  subtract() {
+    this.result = (this.number1) - (this.number2);
+  }
+
+  divide() {
+    this.result = (this.number1) / (this.number2);
+  }
+
+  multiply() {
+    this.result = (this.number1) * (this.number2);
   }
 
 }
